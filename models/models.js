4f1c30d51c6c3605d759b4b312e73f1d34e53580
José Luna <joseluna@uan.edu.mx>
2015-07-31 13:23:20 -0600

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -1,13 +1,13 @@
 var path = require('path');
 
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var DB_name = (url[6]||null);
-var user = (url[2]||null);
-var pwd = (url[3]||null);
-var protocol = (url[1]||null);
-var dialect = (url[1]||null);
-var port = (url[5]||null);
-var host = (url[4]||null);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
 var storage = process.env.DATABASE_STORAGE;
 
 //Cargar Modelo ORM
@@ -30,8 +30,7 @@ var sequelize = new Sequelize(DB_name, user, pwd, {
     host: host,
     storage: storage,
     omitNull: true
-}
-                              );
+});
 
 // Importar la definición de la tabla Quiz en quiz.jz
 
@@ -46,8 +45,12 @@ sequelize.sync().then(function () {
     Quiz.count().then(function (count) {
         if (count === 0) { //la tabla se inicializa cuando no existe
             Quiz.create({
-                    pregunta: 'Capital de Italia',
-                    respuesta: 'Roma'
+                pregunta: 'Capital de Italia',
+                respuesta: 'Roma'
+            });
+            Quiz.create({
+                    pregunta: 'Capital de Portugal',
+                    respuesta: 'Lisboa'
                 })
                 .then(function () {
                     console.log('Base de datos arrancada')
