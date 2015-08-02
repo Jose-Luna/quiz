bfd10400e0db7b0e9ca501fb403395168af706f3
José Luna <joseluna@uan.edu.mx>
2015-08-01 19:39:16 -0600

    Validaciones

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -22,7 +22,7 @@ exports.index = function (req, res) {
 
         models.Quiz.findAll().then(function (quizes) {
             res.render('quizes/index.ejs', {
-                quizes: quizes
+                quizes: quizes, errors: []
             });
         }).catch(function (error) {
             next(error);
@@ -63,6 +63,7 @@ exports.show = function (req, res) {
         })*/
     res.render('quizes/show', {
         quiz: req.quiz
+        , errors: []
     });
 };
 
@@ -102,28 +103,39 @@ exports.answer = function (req, res) {
     res.render('quizes/answer', {
         quiz: req.quiz,
         respuesta: resultado
+        , errors: []
     });
 };
 
 
 // GET /quizes/new
-exports.new = function(req, res) {
+exports.new = function (req, res) {
     var quiz = models.Quiz.build({
         pregunta: "Pregunta",
         respuesta: "Respuesta"
     });
     res.render('quizes/new', {
         quiz: quiz
+        , errors: []
     });
 };
 
 exports.create = function (req, res) {
     var quiz = models.Quiz.build(req.body.quiz);
 
-    quiz.save({
-        fields: ["pregunta", "respuesta"]
-    }).then(function () {
-        res.redirect('/quizes');
-    })
+    quiz.validate().then(function (err) {
+        if (err) {
+            res.render("quizes/new", {
+                quiz: quiz,
+                errors: err.errors
+            });
+        } else {
+            quiz.save({
+                fields: ["pregunta", "respuesta"]
+            }).then(function () {
+                res.redirect('/quizes');
+            })
+        }
 
+    });
 };
--- models/quiz.js
+++ models/quiz.js
@@ -1,8 +1,20 @@
 // Definición del modelo de QUiz
 
-module.exports =function(sequelize, DataTypes) {
-    return sequelize.define('Quiz',
-                            {   pregunta: DataTypes.STRING,
-                                respuesta: DataTypes.STRING
-                            });
+module.exports = function (sequelize, DataTypes) {
+    return sequelize.define('Quiz', {
+        pregunta: {
+            type: DataTypes.STRING, validate: {
+                notEmpty: {
+                    msg: "Falta el contenido de la pregunta"
+                }
+            }
+        },
+        respuesta: {
+            type: DataTypes.STRING, validate: {
+                notEmpty: {
+                    msg: "Falta el contenido de la respuesta"
+                }
+            }
+        }
+    });
 }
